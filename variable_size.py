ce2e1c046c3f83c899ff7878c1b2463a5e8156a6
Rao Muhammad Dayan Atif <162185730+dayanatif@users.noreply.github.com>
2024-10-20 11:05:00 +0500

    Add files via upload

---
+++ variable_size.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        charSet = set()
+        l = 0
+
+        count = 0
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            count = max(count, r - l + 1)
+        return count
